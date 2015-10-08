3a1ab9f0734ced99eba1c05fbfd914138fbee89c
yannick <yk.mukenge@gmail.com>
2015-10-08 18:01:46 +0200

    Créa entite pdt et form créa pdt

--- src/Wa/BackBundle/Controller/ProduitController.php
+++ src/Wa/BackBundle/Controller/ProduitController.php
@@ -3,6 +3,9 @@
 namespace Wa\BackBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Wa\BackBundle\Entity\Produit;
+
 //use Symfony\Component\HttpFoundation\Response;
 
 class ProduitController extends Controller
@@ -81,5 +84,30 @@ class ProduitController extends Controller
         ];
 
         return $this->render('WaBackBundle:Produit:list.html.twig', array('products' => $products));
+    }
+
+    public function createAction(Request $request){
+        $produit = new Produit();
+        //$produit->setTitle('Hello');
+        $formProduit = $this->createFormBuilder($produit)
+                            ->add("title","text")
+                            ->add("description","textarea")
+                            ->add("price")
+                            ->add("quantity")
+                            ->add("envoyer","submit")
+                            ->getForm();
+
+        //dump($produit);
+        //Recupère tt ce que le user tape dans le form
+        $formProduit->handleRequest($request);
+        if($formProduit->isValid()){
+            //die(dump($produit));
+            //On récupère la cnx° (Doctrine)
+            $em= $this->getDoctrine()->getManager();
+            //Surveille l'objet et ses modifications
+            $em->persist($produit);
+            $em->flush();
+        }
+        return $this->render('WaBackBundle:Produit:formProduit.html.twig', ["formulaireProduit"=>$formProduit->createView()]);
     }
 }
---
+++ src/Wa/BackBundle/Entity/Produit.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace Wa\BackBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Produit
+ *
+ * @ORM\Table(name="produit")
+ * @ORM\Entity(repositoryClass="Wa\BackBundle\Entity\ProduitRepository")
+ */
+class Produit
+{
+     /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=100)
+     */
+    /**
+     * @Assert\NotBlank(message="Remplir champs titre")
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="price", type="float")
+     */
+    private $price;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_created", type="datetime")
+     */
+    private $dateCreated;
+
+    /**
+     * @var \Integer
+     *
+     * @ORM\Column(name="quantity", type="integer", options={"default"=1})
+     */
+    private $quantity;
+
+
+
+    public function __construct(){
+        $this->dateCreated= new \Datetime("now");
+        $this->quantity= 1;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Produit
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Produit
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     *
+     * @return Produit
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set dateCreated
+     *
+     * @param \DateTime $dateCreated
+     *
+     * @return Produit
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreated
+     *
+     * @return \DateTime
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     *
+     * @return Produit
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return integer
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+}
